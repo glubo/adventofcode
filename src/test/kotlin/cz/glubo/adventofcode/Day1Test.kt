b13d61632278f0ba43d43582e0b0984191cfd8ef
Petr Sykora <petr.sykora@gmail.com>
2022-12-06 10:33:34 +0100

    Initial commit

---
+++ src/test/kotlin/cz/glubo/adventofcode/Day1Test.kt
@@ -0,0 +1,11 @@
+package cz.glubo.adventofcode
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day1Test {
+    @Test
+    fun `can we run a test`(){
+        Assertions.assertTrue(true, "We can run test")
+    }
+}
