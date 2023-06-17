706dd9bf6205fdfb7e9c57a21429f5dca0fcd45f
Petr Sykora <petr.sykora@gmail.com>
2023-06-17 11:33:27 +0200

    Add refreshVersions to help with keeping dependencies up to date

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.21"
-    kotlin("kapt") version "1.7.21"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    kotlin("jvm")
+    kotlin("kapt")
+    id("com.github.johnrengelman.shadow")
     application
 }
 
@@ -16,12 +16,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
-    implementation("info.picocli:picocli:4.7.0")
-    kapt("info.picocli:picocli-codegen:4.7.0")
+    implementation(KotlinX.coroutines.core)
+    implementation("info.picocli:picocli:_")
+    kapt("info.picocli:picocli-codegen:_")
     testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
-    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testImplementation(KotlinX.coroutines.test)
+    testImplementation(Testing.junit.jupiter.params)
 }
 
 tasks.test {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+import de.fayard.refreshVersions.core.StabilityLevel.Stable
 
+plugins {
+    id("de.fayard.refreshVersions") version "0.51.0"
+}
 rootProject.name = "adventofcode"
 
