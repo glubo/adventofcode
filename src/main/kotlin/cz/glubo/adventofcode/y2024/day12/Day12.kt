4a12699468f927a985e452179f8ff5338466538b
Petr Sykora <petr.sykora@gmail.com>
2024-12-12 23:39:21 +0100

    refactor: day 12 clean up and count corners

--- src/main/kotlin/cz/glubo/adventofcode/y2024/day12/Day12.kt
+++ src/main/kotlin/cz/glubo/adventofcode/y2024/day12/Day12.kt
@@ -13,16 +13,33 @@ data class Tile(
     var used: Boolean = false,
 )
 
-fun fillAndCount(
+fun fillAndCountPricePart1(
     grid: Grid<Tile>,
     pos: IVec2,
 ): Long {
-    val all = mutableListOf<IVec2>()
-    val heads = mutableListOf(pos)
+    val allConnectedTiles = flood(pos, grid)
+
+    val area = allConnectedTiles.size
+    val circumference =
+        allConnectedTiles.sumOf { a ->
+            Direction.entries.sumOf {
+                if (allConnectedTiles.contains(a + it.vector)) 0L else 1L
+            }
+        }
+
+    return area * circumference
+}
+
+private fun flood(
+    start: IVec2,
+    grid: Grid<Tile>,
+): MutableList<IVec2> {
+    val allConnectedTiles = mutableListOf<IVec2>()
+    val heads = mutableListOf(start)
 
     while (heads.isNotEmpty()) {
         val head = heads.removeFirst()
-        all.add(head)
+        allConnectedTiles.add(head)
         val currentTile = grid[head]!!
         currentTile.used = true
 
@@ -33,28 +50,12 @@ fun fillAndCount(
                 grid[newPosition]!!.used == true -> return@forEach
                 grid[newPosition]!!.value != currentTile.value -> return@forEach
                 heads.contains(newPosition) -> return@forEach
-                all.contains(newPosition) -> return@forEach
+                allConnectedTiles.contains(newPosition) -> return@forEach
                 else -> heads.add(newPosition)
             }
         }
     }
-    val area = all.size
-    val circumference =
-        all.sumOf { a ->
-            Direction.entries.sumOf {
-                if (all.contains(a + it.vector)) 0L else 1L
-            }
-        }
-
-    logger.debug {
-        val a = grid[all.first()]?.value
-        "$a $area * $circumference"
-    }
-    logger.debug { all }
-    grid.debug { it ->
-        if (it.used) it.value.uppercaseChar() else it.value.lowercaseChar()
-    }
-    return area * circumference
+    return allConnectedTiles
 }
 
 suspend fun y2024day12part1(input: Input): Long {
@@ -63,7 +64,7 @@ suspend fun y2024day12part1(input: Input): Long {
     var sum = 0L
     grid.allIVec2().forEach { pos ->
         if (grid[pos]?.used == false) {
-            sum += fillAndCount(grid, pos)
+            sum += fillAndCountPricePart1(grid, pos)
         }
     }
     return sum
@@ -71,59 +72,29 @@ suspend fun y2024day12part1(input: Input): Long {
 
 fun fillAndCount2(
     grid: Grid<Tile>,
-    pos: IVec2,
+    start: IVec2,
 ): Long {
-    val all = mutableListOf<IVec2>()
-    val heads = mutableListOf(pos)
+    val allConnectedTiles = flood(start, grid)
 
-    while (heads.isNotEmpty()) {
-        val head = heads.removeFirst()
-        all.add(head)
-        val currentTile = grid[head]!!
-        currentTile.used = true
+    allConnectedTiles.sortBy { it.x + it.y }
+    val area = allConnectedTiles.size
 
-        Direction.entries.forEach { dir ->
-            val newPosition = head + dir.vector
-            when {
-                grid[newPosition] == null -> return@forEach
-                grid[newPosition]!!.used == true -> return@forEach
-                grid[newPosition]!!.value != currentTile.value -> return@forEach
-                heads.contains(newPosition) -> return@forEach
-                all.contains(newPosition) -> return@forEach
-                else -> heads.add(newPosition)
+    val perimeter =
+        buildSet {
+            allConnectedTiles.forEach { pos ->
+                Direction.entries.forEach { dir ->
+                    if ((pos + dir.vector) !in allConnectedTiles) add(dir to pos)
+                }
             }
         }
-    }
-    all.sortBy { it.x + it.y }
-    val area = all.size
-    val sides = mutableListOf<Pair<Direction, MutableList<IVec2>>>()
-    all.forEach { currentPos ->
-        Direction.entries.forEach { sideDirection ->
-            if (all.contains(currentPos + sideDirection.vector)) return@forEach
-            val possibleSameSides =
-                sideDirection
-                    .perpendicular()
-                    .map { currentPos + it.vector }
-                    .map { sideDirection to it }
-            val sameSide =
-                possibleSameSides
-                    .map { pss ->
-                        sides.firstOrNull { s ->
-                            (pss.first == s.first) && s.second.any { pss.second == it }
-                        }
-                    }.firstOrNull { it !== null }
 
-            if (sameSide != null) {
-                sameSide.second.add(currentPos)
-            } else {
-                sides.add(sideDirection to mutableListOf(currentPos))
-            }
-        }
-    }
+    val corners =
+        perimeter
+            .filterNot { (dir, pos) ->
+                (dir to pos + dir.turnRight().vector) in perimeter
+            }.map { it.second }
 
-    val circumference = sides.size
-
-    return area.toLong() * circumference
+    return area.toLong() * corners.size
 }
 
 suspend fun y2024day12part2(input: Input): Long {
